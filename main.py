97865f28f0456781986c0447d70c0eaa787dd0d2
Gabriel <Gabvas2010@gmail.com>
2025-11-15 20:54:38 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# it's a ressource manangement game for the numworks
+import ion
+import kandinsky as ks
+
+#======================================================================================
+def mainloop():
+    ressources : list = []
